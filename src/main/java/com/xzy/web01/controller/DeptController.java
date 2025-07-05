941a052e9d673c5b28b857f7f99e9fb1c0d45d26
谁解幽人幽意 <2634118472@qq.com>
2025-07-06 00:10:41 +0800

    增加DeptController类

--- src/main/java/com/xzy/web01/controller/DeptController.java
+++ src/main/java/com/xzy/web01/controller/DeptController.java
@@ -1,4 +1,10 @@
 package com.xzy.web01.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dept")
 public class DeptController {
 }
