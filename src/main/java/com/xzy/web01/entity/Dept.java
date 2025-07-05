2373a77dfa256ed62a74ad12d5b97d75d22ac01a
谁解幽人幽意 <2634118472@qq.com>
2025-07-05 23:56:29 +0800

    增加实体类Dept Result

--- src/main/java/com/xzy/web01/entity/Dept.java
+++ src/main/java/com/xzy/web01/entity/Dept.java
@@ -5,9 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Dept {
-
+    private Integer id;
+    private String name;
+    private LocalDateTime create_time;
+    private LocalDateTime update_time;
 }
---
+++ src/main/java/com/xzy/web01/entity/Result.java
@@ -0,0 +1,37 @@
+package com.xzy.web01.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+    private Integer code;   // 错误码 1-success 0-error
+    private String msg;     // 错误信息
+    private Object data;    // 实体数据
+
+    // 生成成功的消息
+    public static Result success(){
+        Result result = new Result();
+        result.setCode(1);
+        result.setMsg("success");
+        return result;
+    }
+
+    public static Result success(Object data){
+        Result result = success();
+        result.setData(data);
+        return result;
+    }
+
+    public static Result error(){
+        Result result = new Result();
+        result.setCode(0);
+        result.setMsg("error");
+        return result;
+    }
+
+}
