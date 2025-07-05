a1ab329960909bed13e80939229ec61b820addac
谁解幽人幽意 <2634118472@qq.com>
2025-07-05 23:50:03 +0800

    修正配置名

---
+++ src/main/java/com/xzy/web01/entity/Dept.java
@@ -0,0 +1,13 @@
+package com.xzy.web01.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dept {
+
+}
