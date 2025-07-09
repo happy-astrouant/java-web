a9ead436fc0814706557e6884881acdd14487889
谁解幽人幽意 <2634118472@qq.com>
2025-07-09 11:26:21 +0800

    update

--- src/main/java/com/xzy/web01/service/impl/EmpLogServiceImpl.java
+++ src/main/java/com/xzy/web01/service/impl/EmpLogServiceImpl.java
@@ -5,6 +5,8 @@ import com.xzy.web01.mapper.EmpLogMapper;
 import com.xzy.web01.service.EmpLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmpLogServiceImpl implements EmpLogService {
@@ -12,7 +14,9 @@ public class EmpLogServiceImpl implements EmpLogService {
     @Autowired
     EmpLogMapper empLogMapper;
 
+    // 默认属性为REQUIRED，即自动与上级事务合并，设置为REQUIRES_NEW，单独创建事务
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void insert(EmpLog empLog) {
         empLogMapper.insert(empLog);
     }
