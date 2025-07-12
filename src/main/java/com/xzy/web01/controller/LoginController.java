90aabf994e0c7c7277e06567dc70211bcfc85672
谁解幽人幽意 <2634118472@qq.com>
2025-07-12 16:37:54 +0800

    创建新的数据库

--- src/main/java/com/xzy/web01/controller/LoginController.java
+++ src/main/java/com/xzy/web01/controller/LoginController.java
@@ -6,7 +6,11 @@ import com.xzy.web01.entity.Result;
 import com.xzy.web01.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -14,8 +18,10 @@ public class LoginController {
     @Autowired
     private EmpService empService;
 
-    @GetMapping("/login")
-    public Result login(String username, String password) {
+    @PostMapping("/login")
+    public Result login(@RequestBody Map<String, Object> param) {
+        String username = (String) param.get("username");
+        String password = (String) param.get("password");
         LoginInfo loginInfo = empService.login(username, password);
         if(loginInfo==null){
             return Result.error("用户名或密码错误");
